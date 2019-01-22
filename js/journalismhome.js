d1b29ab1b849deed0d8a535f966a39c0c84f1813
aallen121 <44952800+aallen121@users.noreply.github.com>
2019-01-22 14:39:58 -0600

    Update journalismhome.js

--- js/journalismhome.js
+++ js/journalismhome.js
@@ -9,14 +9,17 @@ else {document.getElementById("nav1").style.display = "none";
 //Show menu when in full screen
 function dis() {
   if ($(window).width()>817){
-    document.getElementById("nav1").style.display = "inline-block";}
-  else{document.getElementById("nav1").style.display = "none";}
+    document.getElementById("nav1").style.display = "inline-block";
+    document.getElementById("main-section").style.opacity = 1;
+    }
+  else{document.getElementById("nav1").style.display = "none";
+  document.getElementById("main-section").style.opacity = 1;}
 }
 //Close menu if resize to mobile
 function setview() {
   if ($(window).width()<817){
     document.getElementById("nav1").style.display = "none";}
-  else {document.getElementbyId("nav1").style.display = "inline-block"}
+  else {document.getElementbyId("nav1").style.display = "inline-block";}
 }
 //Close menu if click off to side
 function closemenu() {
@@ -25,7 +28,6 @@ function closemenu() {
     document.getElementById("main-section").style.opacity = 1;
   }
 }
-
 
 //Resize portrait-orientation Photos
 //function resz() {
