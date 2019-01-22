ac068b0d17445070636229dfd76cfda1f5233fd4
aallen121 <44952800+aallen121@users.noreply.github.com>
2019-01-22 13:36:15 -0600

    Update journalismhome.js

--- js/journalismhome.js
+++ js/journalismhome.js
@@ -1,25 +1,39 @@
 //Open and close menu in mobile
 function menu() {
-  if (document.getElementById("nav1").style.display=="none"){
-      document.getElementById("nav1").style.display="inline-block";}
-else {document.getElementById("nav1").style.display="none";}
+  if (document.getElementById("nav1").style.display == "none") {
+      document.getElementById("nav1").style.display = "inline-block";
+      document.getElementById("main-section").style.opacity = .5;}
+else {document.getElementById("nav1").style.display = "none";
+      document.getElementById("main-section").style.opacity = 1;}
 }
 //Show menu when in full screen
-function dis(){
+function dis() {
   if ($(window).width()>817){
-    document.getElementById("nav1").style.display="inline-block";
-  }
-  else{document.getElementById("nav1").style.display="none";}
+    document.getElementById("nav1").style.display = "inline-block";}
+  else{document.getElementById("nav1").style.display = "none";}
 }
 //Close menu if resize to mobile
-function setview(){
+function setview() {
   if ($(window).width()<817){
-    document.getElementById("nav1").style.display="none";
-  }
+    document.getElementById("nav1").style.display = "none";}
+  else {document.getElementbyId("nav1").style.display = "inline-block"}
 }
 //Close menu if click off to side
-//function closemenu(){
-//  if (document.getElementById("nav1").style.display="inline-block"){
-//    document.getElementById("nav1").style.display="none");
-//  }
+function closemenu() {
+  if (document.getElementById("nav1").style.display == "inline-block") {
+    document.getElementById("nav1").style.display = "none";
+    document.getElementById("main-section").style.opacity = 1;
+  }
+}
+
+
+//Resize portrait-orientation Photos
+//function resz() {
+//  portraitphoto = document.getElementsbyTagName("img")
+//  if (portraitphoto.style.height > 600) {
+//      portraitphoto.style.display = "block";
+//      portraitphoto.style.width = 50%;
+//      portraitphoto.style.marginleft = "auto";
+//      portraitphoto.style.marginright = "auto";}
+//  else{document.getElementsbyTagName("img").style.width=100%}
 //}
